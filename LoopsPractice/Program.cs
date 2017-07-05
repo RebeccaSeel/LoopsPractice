5334d85b6a506c7da1617ba6a1324abdacca5f22
rebeccaseel <rseelgws@gmail.com>
2017-07-05 15:15:27 -0400

    reviewed concepts

--- LoopsPractice/Program.cs
+++ LoopsPractice/Program.cs
@@ -10,6 +10,23 @@ namespace LoopsPractice
     {
         static void Main(string[] args)
         {
+            //loops: for loops, foreach loops, while loops, do-while loops
+            //when would you use a loop? - repeition of a fragment of code
+            //why would you use a loop?
+            //nested loops ex. clock - seconds, minutes, hours, days
+            //infinite loops
+            //keywords break and continue
+
+            //For Loop - for counting  and ... iterating (going from one index to next, etc., doing same activity) 
+            //through an array (or other collection type)
+            //Look like this:
+            //for(initializer; condition; updater)
+            //{
+            //  do something
+            //}
+
+
+
         }
     }
 }
